0b92a7931396ec9eb3b853e54d1eedfb69e73e5e
Your Name <you@example.com>
2020-08-18 15:24:10 +0900

    a

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,23 +1,30 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import org.springframework.stereotype.Controller;
 import com.example.demo.entity.Pay;
 import com.example.demo.mapper.PayMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import com.example.util.LogUtils;
 
-@Controller
+@RestController
 public class IndexController {
 	@Autowired
 	PayMapper payMapper;
 	
-	@RequestMapping
-	public String index(Model model) {
+	@RequestMapping(value="gettest",method=RequestMethod.GET)
+	public List<Pay> index(@ModelAttribute Inputter inp) {
 		List<Pay> list = payMapper.selectAll();
-		model.addAttribute("pay",list);
-		return "index";
+		//LogUtils.trace("trace");
+		//LogUtils.debug("debug");
+		LogUtils.info(inp.toString());
+		//LogUtils.warn("warn");
+		//LogUtils.error("error");
+		//model.addAttribute("pay",list);
+		return list;
 	}
 	
 }
---
+++ src/main/java/com/example/demo/controller/Inputter.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+import lombok.Data;
+
+@Data
+public class Inputter {
+	String messageID;
+	String payId;
+}
